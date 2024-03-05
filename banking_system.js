b6b2e5fa1095fb9ebda9cfec95f497487613d860
rizkirobawa <rizkirobawa@gmail.com>
2024-03-05 15:04:12 +0700

    Handling error function

--- banking_system.js
+++ banking_system.js
@@ -6,13 +6,15 @@ class BankAccount {
   deposit(amount) {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
-        if (amount <= 0 && isNaN(amount)) {
-          return reject("Wrong input!");
-        } else {
+        if (amount > 0 && !isNaN(amount)) {
           this.balance += amount;
           resolve(
             `Anda berhasil melakukan deposit sebesar ${amount}. Total Saldo Anda sekarang adalah : ${this.balance}`
           );
+        } else if (isNaN(amount)) {
+          return reject("Wrong input!");
+        } else {
+          return reject("Saldo yang diinput tidak boleh 0");
         }
       }, 3000);
     });
@@ -21,14 +23,18 @@ class BankAccount {
   withdraw(amount) {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
-        if (isNaN(amount)) {
-          return reject("Wrong input!");
-        } else if (amount >= this.balance) {
-          return reject("Saldo Anda tidak mencukupi untuk melakukan withdraw");
-        } else {
+        if (!isNaN(amount) && amount > 0 && amount <= this.balance) {
           this.balance -= amount;
           resolve(
-            `Anda berhasil melakukan withdraw sebesar ${amount}. Total Saldo Anda sekarang adalah : ${this.balance}`
+            `Anda berhasil melakukan deposit sebesar ${amount}. Total Saldo Anda sekarang adalah : ${this.balance}`
+          );
+        } else if (isNaN(amount)) {
+          return reject("Wrong input!");
+        } else if (amount <= 0) {
+          return reject("Saldo yang diinput tidak boleh 0");
+        } else {
+          return reject(
+            `Saldo yang diinput tidak boleh lebih besar dari saldo awal!`
           );
         }
       }, 3000);
@@ -41,9 +47,13 @@ const myAccount = new BankAccount();
 async function transaction() {
   try {
     console.log(`Selamat Datang di BanK root`);
-    console.log(await myAccount.deposit(100));
-    console.log(await myAccount.withdraw(50));
-    console.log(`Transaksi selesai. Menunggu beberapa saat sebelum keluar...`);
+    console.log(await myAccount.deposit(12000));
+    console.log(await myAccount.withdraw(10000));
+    setTimeout(() => {
+      console.log(
+        `Transaksi selesai. Menunggu beberapa saat sebelum keluar...`
+      );
+    }, 2000);
   } catch (error) {
     console.log(error);
   }
@@ -53,5 +63,5 @@ async function transaction() {
   await transaction();
   setTimeout(() => {
     console.log(`Terima Kasih`);
-  }, 4000);
+  }, 5000);
 })();
